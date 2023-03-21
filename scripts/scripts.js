e41900b2a57c45efaea60c10f97a1e93628048e2
Tomasz Dziezyk <tomasz.dziezyk@netcentric.biz>
2023-03-21 10:45:24 +0100

    #16 Remove commented code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -81,11 +81,6 @@ function buildHeroBlock(main) {
 async function loadMoreNews() {
   if (!window.mack.newsData.allLoaded) {
     const queryLimit = 200;
-    /*
-      console.trace();
-      console
-      .log(`loading posts with offset ${window.mack.newsData.offset} and limit ${queryLimit}`);
-    */
     const resp = await fetch(`/mack-news.json?limit=${queryLimit}&offset=${window.mack.newsData.offset}`);
     const json = await resp.json();
     const { total, data } = json;
