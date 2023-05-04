5666d8f69ee4bd682ec1119a038388c971dc94a9
Aravindo Wingeier <wingeier@adobe.com>
2023-05-04 11:53:06 -0700

    delete news-feed-list.css

--- blocks/news-feed-list/news-feed-list.js
+++
@@ -1,9 +0,0 @@
-export default function decorate(block) {
-  const header = document.createElement('h2');
-  header.classList.add('news-feed-list-header');
-  header.textContent = 'News RSS';
-  const icon = document.createElement('span');
-  icon.className = 'icon feed-icon';
-  header.prepend(icon);
-  block.prepend(header);
-}
