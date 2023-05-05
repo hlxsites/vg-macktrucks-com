5a044009e9fdb83712079987a700f841bed1207a
Aravindo Wingeier <wingeier@adobe.com>
2023-05-04 18:35:20 -0700

    read config

--- blocks/news-list/news-list.js
+++ blocks/news-list/news-list.js
@@ -1,16 +1,19 @@
 import { createElement } from '../../scripts/scripts.js';
 import { getNews } from '../../scripts/news.js';
+import { readBlockConfig } from '../../scripts/lib-franklin.js';
 
 // TODO: make index configurable in block
 export default async function decorate(block) {
-  const rssFeedUrl = block.querySelector('a').href;
+  const config = readBlockConfig(block);
+  const rssFeedUrl = config.feed;
+  const filter = config.filter || 'mack-news';
   block.textContent = '';
 
   const rssLink = createElement('a', ['title-with-icon'], { href: rssFeedUrl, target: '_blank' });
   rssLink.textContent = 'News RSS';
   block.append(rssLink);
 
-  const posts = await getNews('mack-news');
+  const posts = await getNews(filter);
   const list = createElement('ul', ['news-sidebar-list']);
   list.append(...posts.map((post) => {
     const articleLink = createElement('a', [], { href: post.path });
