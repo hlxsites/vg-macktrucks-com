7561ab2c2e224d4188f0cab5b180132d5cd231ec
SantiagoHomps-NC <santiago.homps@netcentric.biz>
2023-05-10 18:06:31 +0200

    block variation 'new' added

--- blocks/download/download.js
+++ blocks/download/download.js
@@ -1,6 +1,6 @@
 import { createElement } from '../../scripts/scripts.js';
 
-export default function decorate(block) {
+const buildRegularDownloadBlock = (block) => {
   const children = [...block.children];
 
   const downloadList = createElement('ul', 'download-list');
@@ -45,4 +45,51 @@ export default function decorate(block) {
   });
   block.textContent = '';
   block.append(downloadList);
+};
+
+const buildNewDownloadBlock = (block) => {
+  const children = [...block.children];
+  const downloadNewList = createElement('ul', 'download-new-list');
+
+  children.forEach((e, idx) => {
+    const downloadItem = createElement('li', ['download-new-item', `download-new-item-${idx + 1}`]);
+    const linkUrl = e.querySelector('a').getAttribute('href');
+    const allTexts = e.querySelectorAll('li');
+
+    const newCondition = e.children[1].innerText.toUpperCase();
+    const number = allTexts[1].innerText;
+    const title = allTexts[2].innerText;
+    const subTitle = allTexts[3].innerText;
+
+    downloadItem.innerHTML = `
+    <a href="${linkUrl}" target="_blank" rel="noopener" class='download-new-link'>
+        <div class='download-new-texts'>
+          <p>
+            ${number}. ${title}
+          </p>
+          <small>
+            ${subTitle}
+          </small>
+        </div>
+    ${newCondition.length === 0 ? ''
+    : `<div class='download-new-label'>
+      ${newCondition}
+    </div>`}
+    </a>`;
+    downloadNewList.append(downloadItem);
+  });
+
+  block.textContent = '';
+  block.append(downloadNewList);
+};
+
+export default function decorate(block) {
+  const typeDetector = [...block.classList];
+
+  // This detects what block needs to be render and calls the corresponding function.
+  if (!typeDetector.includes('new')) {
+    buildRegularDownloadBlock(block);
+  } else if (typeDetector.includes('new')) {
+    buildNewDownloadBlock(block);
+  }
 }
