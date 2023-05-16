f3e02b80bbaa31a77a67801ba84b8ca00fc98742
Aravindo Wingeier <wingeier@adobe.com>
2023-05-16 15:51:39 -0700

    style features

--- blocks/hotspots-features/hotspots-features.js
+++ blocks/hotspots-features/hotspots-features.js
@@ -1,2 +1,15 @@
 export default function decorate(block) {
+  block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((title) => {
+    // convert to h4 because it might be any header level
+    const h4 = document.createElement('h4');
+    h4.classList.add('feature-title');
+    h4.innerHTML = title.innerHTML;
+    title.replaceWith(h4);
+  });
+
+  block.querySelectorAll(':scope > div').forEach((row) => row.classList.add('row'));
+  block.querySelectorAll(':scope > div > div').forEach((cell) => cell.classList.add('feature'));
+
+  // unwrap picture so that text styles are not applied to this paragraph
+  block.querySelectorAll('picture').forEach((picture) => picture.closest('p').replaceWith(picture));
 }
