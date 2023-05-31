3fd412e4d92d04ca64f9f1b1706739179878ed5d
Tomasz Dziezyk <tomasz.dziezyk@netcentric.biz>
2023-05-31 14:25:10 +0200

    #29 Add table block

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,9 +1,17 @@
 import { createElement } from '../../scripts/scripts.js';
+import fragmentBlock from '../fragment/fragment.js';
 
-export default function decorate(block) {
+/* Function checks if the content of the provied element is just a link to other doc */
+function isContentLink(el) {
+  // The assumpions:
+  // 1. The content is just plain text - no HTML inside
+  // 2. The link starts from '/' and doesn't contain any white space character
+  return el.innerHTML === el.textContent && /^\/(\S+)$/g.test(el.innerHTML);
+}
+
+export default async function decorate(block) {
   const rows = [...block.querySelectorAll(':scope > div')];
-
-  const accordions = rows.map((row) => {
+  const accordionsPromises = rows.map(async (row) => {
     const accordionHeader = row.querySelector(
       ':scope > div > h1, :scope > div > h2, :scope > div > h3, :scope > div > h4, :scope > div > h5, :scope > div > h6',
     );
@@ -14,10 +22,15 @@ export default function decorate(block) {
     const headerButton = createElement('button', 'accordion-header-button');
     const headerEl = createElement('h2', 'accordion-header');
     const plusEl = createElement('div', 'accordion-close');
-    headerEl.innerHTML = accordionHeader.innerHTML;
+    headerEl.innerHTML = accordionHeader?.innerHTML;
     headerButton.append(headerEl, plusEl);
 
     const contentEl = createElement('div', ['accordion-content', 'accordion-content-close']);
+
+    if (isContentLink(accordionContent)) {
+      await fragmentBlock(accordionContent);
+    }
+
     contentEl.innerHTML = accordionContent.innerHTML;
 
     const accordionEl = createElement('div', ['accordion-item', 'accordion-item-close']);
@@ -32,5 +45,9 @@ export default function decorate(block) {
   });
 
   block.innerHTML = '';
-  accordions.forEach((acc) => block.append(acc));
+  await Promise.allSettled(accordionsPromises);
+  accordionsPromises.forEach(async (acc) => {
+    const result = await acc;
+    block.append(result);
+  });
 }
