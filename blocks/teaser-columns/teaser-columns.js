a7f2d125f77ccc6e8b0dd89435c0d4800a639db5
Lakshmi <lakshmishri.aswathnarayana@netcentric.biz>
2023-06-01 12:09:33 +0200

    changes for teaser columns

--- blocks/teaser-columns/teaser-columns.js
+++ blocks/teaser-columns/teaser-columns.js
@@ -1,21 +1,17 @@
 import { createElement } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  const textOnImage = block.classList.contains('text-on-image');
-  block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((title) => {
-    // convert to h4 because it might be any header level
-    const h4 = createElement('h4', 'teaser-title');
-    h4.innerHTML = title.innerHTML;
-    title.replaceWith(h4);
-  });
-
   block.querySelectorAll(':scope > div').forEach((row) => row.classList.add('row'));
   block.querySelectorAll(':scope > div > div').forEach((cell) => {
     cell.classList.add('teaser-column');
 
-    if (cell.querySelector('picture') && textOnImage) {
-      console.log('here');
+    if (cell.querySelector('picture')) {
       cell.classList.add('text-on-image-column');
+    } else {
+      const textCol = createElement('div', ['text-wrapper']);
+      textCol.innerHTML = cell.innerHTML;
+      cell.innerHTML = '';
+      cell.appendChild(textCol);
     }
   });
 
