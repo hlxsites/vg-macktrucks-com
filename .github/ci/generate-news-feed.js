beafff480d10d1d24969d2346b77bb7f4d97cd4d
Aravindo Wingeier <wingeier@adobe.com>
2023-06-05 14:40:03 -0700

    feat #55: refactor to use multiple feeds

--- .github/ci/generate-news-feed.js
+++ .github/ci/generate-news-feed.js
@@ -1,57 +1,95 @@
 import { Feed } from 'feed';
 import fs from 'fs';
 
-const endpoint = 'https://main--vg-macktrucks-com--hlxsites.hlx.live/mack-news/feed.json';
-const feedInfoEndpoint = 'https://main--vg-macktrucks-com--hlxsites.hlx.live/mack-news/feed-info.json';
-const targetDirectory = '../../mack-news';
-const targetFile = `${targetDirectory}/feed.xml`;
-const limit = 1000;
+/**
+ * @type {FeedConfig[]}
+ */
+const feeds = [
+  {
+    title: 'Mack News',
+    targetFile: `../../mack-news/feed.xml`,
+    source: 'https://main--vg-macktrucks-com--hlxsites.hlx.live/mack-news/feed.json',
+    siteRoot: "https://www.macktrucks.com",
+    link:	"https://www.macktrucks.com/mack-news/",
+    language:	"en",
+    description:	"Get the latest news from Mack® Trucks and see how we are taking our Born Ready " +
+      "semi truck line to the next level with new innovations and technology."
+  },
+]
 
-async function main() {
-  const allPosts = await fetchBlogPosts();
+
+const limit = "1000";
+
+/**
+ * @typedef {Object} FeedConfig
+ * @property {string} title
+ * @property {string} description
+ * @property {string} link
+ * @property {string} siteRoot
+ * @property {string} targetFile
+ * @property {string} source
+ * @property {string} language
+ */
+
+
+/**
+ * @typedef {Object} Post
+ * @property {string} title
+ * @property {string} summary
+ * @property {string} path
+ * @property {string} publicationDate
+ * @property {string} template
+ */
+
+/**
+ * @param feed {FeedConfig}
+ * @return {Promise<void>}
+ */
+async function createFeed(feed) {
+  const allPosts = await fetchBlogPosts(feed);
   console.log(`found ${allPosts.length} posts`);
 
-  const feedMetadata = await fetchBlogMetadata();
 
   const newestPost = allPosts
     .map((post) => new Date(post.publicationDate * 1000))
     .reduce((maxDate, date) => (date > maxDate ? date : maxDate), new Date(0));
 
-  const feed = new Feed({
-    title: feedMetadata.title,
-    description: feedMetadata.description,
-    id: feedMetadata.link,
-    link: feedMetadata.link,
+  const atomFeed = new Feed({
+    title: feed.title,
+    description: feed.description,
+    id: feed.link,
+    link: feed.link,
     updated: newestPost,
-    generator: 'AEM News feed generator (GitHub action)',
-    language: feedMetadata.lang,
+    generator: 'AEM Project Franklin News feed generator (GitHub action)',
+    language: feed.language,
   });
 
   allPosts.forEach((post) => {
-    const link = feedMetadata["site-root"] + post.path;
-    feed.addItem({
+    const link = feed.siteRoot + post.path;
+    atomFeed.addItem({
       title: post.title,
       id: link,
       link,
       content: post.summary,
       date: new Date(post.publicationDate * 1000),
-      published: new Date(post.publishDate * 1000),
+      published: new Date(post.publicationDate * 1000),
     });
   });
 
-  if (!fs.existsSync(targetDirectory)) {
-    fs.mkdirSync(targetDirectory);
-  }
-  fs.writeFileSync(targetFile, feed.atom1());
-  console.log('wrote file to ', targetFile);
+  fs.writeFileSync(feed.targetFile, atomFeed.atom1());
+  console.log('wrote file to ', feed.targetFile);
 }
 
-async function fetchBlogPosts() {
+/**
+ * @param feed {FeedConfig}
+ * @return {Promise<Post[]>}
+ */
+async function fetchBlogPosts(feed) {
   let offset = 0;
   const allPosts = [];
 
   while (true) {
-    const api = new URL(endpoint);
+    const api = new URL(feed.source);
     api.searchParams.append('offset', JSON.stringify(offset));
     api.searchParams.append('limit', limit);
     const response = await fetch(api, {});
@@ -69,11 +107,8 @@ async function fetchBlogPosts() {
   return allPosts;
 }
 
-async function fetchBlogMetadata() {
-  const infoResponse = await fetch(feedInfoEndpoint);
-  const feedInfoResult = await infoResponse.json();
-  return feedInfoResult.data[0];
-}
+for (const feed of feeds) {
+  createFeed(feed)
+    .catch((e) => console.error(e));
 
-main()
-  .catch((e) => console.error(e));
+}
