db6941b0a8e35bf5740ce836846c970d5c5f8ba9
Tomasz Dziezyk <tomasz.dziezyk@netcentric.biz>
2023-06-07 16:52:12 +0200

    129 Fix iframe size

--- blocks/iframe/iframe.js
+++ blocks/iframe/iframe.js
@@ -2,9 +2,14 @@ export default async function decorate(block) {
   const iframe = document.createElement('iframe');
   const link = block.querySelector('a')?.getAttribute('href');
   const fixedHeightClass = [...block.classList].find((el) => /[0-9]+px/.test(el));
+  const maxWidthClass = [...block.classList].find((el) => /width-[0-9]+px/.test(el));
 
   if (fixedHeightClass) {
     iframe.height = fixedHeightClass;
+  }
+  if (maxWidthClass) {
+    const maxWidht = maxWidthClass.split('width-')[1];
+    iframe.style.maxWidth = maxWidht;
   }
   iframe.src = link;
   iframe.setAttribute('frameborder', 0);
