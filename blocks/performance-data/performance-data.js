39cfea198ff1e1de09b55748e41444250ef441a1
Aravindo Wingeier <wingeier@adobe.com>
2023-06-09 14:17:08 -0700

    extract performance data

--- blocks/performance-data/performance-data.js
+++ blocks/performance-data/performance-data.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 import { readBlockConfig } from '../../scripts/lib-franklin.js';
 
 /**
@@ -7,4 +8,30 @@ import { readBlockConfig } from '../../scripts/lib-franklin.js';
  */
 export default async function decorate(block) {
   const config = readBlockConfig(block);
+
+  let rpmIndex;
+  [...block.children].forEach((row, index) => {
+    if (row.firstElementChild.textContent === 'RPM') {
+      rpmIndex = index;
+    }
+  });
+
+  const performanceData = {};
+  const titles = [];
+  for (let i = 1; i < block.children[rpmIndex].children.length; i++) {
+    // skipping the first column, which is the RPM column
+    titles[i] = block.children[rpmIndex].children[i].textContent;
+    performanceData[titles[i]] = [];
+  }
+  // starting with RPM, remove all rows until the end of the table
+  for (let i = rpmIndex + 1; i < block.children.length; i++) {
+    const row = block.children[i];
+    const rpm = row.children[0].textContent;
+    for (let j = 1; j < row.children.length; j++) {
+      const metric = titles[j];
+      performanceData[metric].push([rpm, row.children[j].textContent]);
+    }
+  }
+  console.log(performanceData);
+  // rpmIndex =
 }
