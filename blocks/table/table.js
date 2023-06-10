59542d5a373425353f00198712747fb880bff52b
Manuel Vara <manuel.vara@netcentric.biz>
2023-06-10 14:27:59 +0200

    Adding row-header variation

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -8,9 +8,22 @@ export default async function decorate(block) {
     const newHeading = createElement('h5', 'table-cell-heading');
     newHeading.innerHTML = heading.innerHTML;
     heading.replaceWith(newHeading);
-
     newHeading.closest('tr')?.classList.add('table-heading');
   });
+
+  const isRowHeader = block.classList.contains('row-header');
+  if (isRowHeader) {
+    const firstColumn = block.querySelectorAll('table td:first-child');
+    firstColumn.forEach((cell) => {
+      const newP = createElement('p');
+      newP.textContent = cell.querySelector('strong').textContent;
+      cell.replaceChild(newP, cell.querySelector('strong'));
+      cell.addEventListener('click', () => {
+        cell.classList.toggle('open');
+        cell.nextElementSibling.classList.toggle('open');
+      });
+    });
+  }
 
   return block;
 }
