888e091d80714461b70e945c078b773378532d71
Aravindo Wingeier <wingeier@adobe.com>
2023-06-09 18:14:39 -0700

    use dynamic import

--- blocks/performance-data/performance-data.js
+++ blocks/performance-data/performance-data.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-plusplus */
-import * as echarts from '../../common/echarts/echarts.esm.js';
 
 /**
  * content of the tabs for the performance-specifications block.
@@ -24,40 +23,40 @@ export default async function decorate(block) {
     keyFacts.appendChild(domEl('dd', row[1]));
   });
 
-
   block.innerText = '';
   block.append(keyFacts);
 
-  const diagram = div( { class: 'performance-chart' });
+  const diagram = div({ class: 'performance-chart' });
   block.append(diagram);
 
-  const myChart = echarts.init(diagram);
+  import('../../common/echarts/echarts.esm.js').then((echarts) => {
+    const myChart = echarts.init(diagram);
 
-  // Specify the configuration items and data for the chart
-  const option = {
-    title: {
-      text: 'ECharts Getting Started Example',
-    },
-    tooltip: {},
-    legend: {
-      data: ['sales'],
-    },
-    xAxis: {
-      data: ['Shirts', 'Cardigans', 'Chiffons', 'Pants', 'Heels', 'Socks'],
-    },
-    yAxis: {},
-    series: [
-      {
-        name: 'sales',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20],
+    // Specify the configuration items and data for the chart
+    const option = {
+      title: {
+        text: 'ECharts Getting Started Example',
       },
-    ],
-  };
+      tooltip: {},
+      legend: {
+        data: ['sales'],
+      },
+      xAxis: {
+        data: ['Shirts', 'Cardigans', 'Chiffons', 'Pants', 'Heels', 'Socks'],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: 'sales',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    };
 
-  // Display the chart using the configuration items and data just specified.
-  myChart.setOption(option);
-
+    // Display the chart using the configuration items and data just specified.
+    myChart.setOption(option);
+  });
 }
 
 export function deconstructBlockIntoArray(blockEl) {
