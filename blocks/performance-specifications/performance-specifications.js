0e5908fcfca50302d849979e706949c6de6b9949
Aravindo Wingeier <wingeier@adobe.com>
2023-06-13 01:10:03 -0700

    hide unless first

--- blocks/performance-specifications/performance-specifications.js
+++ blocks/performance-specifications/performance-specifications.js
@@ -1,5 +1,7 @@
 import { readBlockConfig } from '../../scripts/lib-franklin.js';
-import { div } from '../performance-data/performance-data.js';
+import {
+  div, li, p, ul,
+} from '../performance-data/performance-data.js';
 
 function getModelKey(el) {
   return el.textContent.replaceAll('®', '')
@@ -28,19 +30,37 @@ export default async function decorate(block) {
 
   // prepare tab panels to be filled later
   [...tabList.querySelectorAll('.name')].map((el) => getModelKey(el)).forEach((name, index) => {
-    const panel = div({
-      id: `panel-${name}`,
-      role: 'tabpanel',
-      tabindex: index,
-      'aria-labelledby': 'tab-1',
-    });
+    const panel = div(
+      {
+        id: `panel-${name}`,
+        role: 'tabpanel',
+        tabindex: index,
+        'aria-labelledby': `tab-${name}`,
+      },
+      p({ class: 'title-vertical-tabs' }, 'Engine Ratings'),
+      ul({ class: 'vertical-tabs', role: 'tablist', ariaLabel: 'Engine Ratings' }),
+    );
+    if (index !== 0) panel.setAttribute('hidden', '');
     block.append(panel);
   });
 }
 
 export function addPerformanceData(element) {
   const block = element.closest('.performance-specifications-container').querySelector('.performance-specifications');
-  const modelKey = [...element.classList].find((c) => c !== 'performance-data' && !c.endsWith('HP'));
+  const modelKey = [...element.classList].find((c) => c !== 'performance-data' && !c.toLowerCase().endsWith('-hp') && c !== 'block');
+  const horsepower = [...element.classList].find((c) => c.toLowerCase().endsWith('-hp'));
   const panel = block.querySelector(`#panel-${modelKey}`);
+  element.setAttribute('role', 'tabpanel');
+  element.setAttribute('tabindex', panel.children.length);
+  element.setAttribute('aria-labelledby', `tab-${modelKey}-${horsepower}`);
+  if (panel.querySelectorAll('.performance-data').length !== 0) element.setAttribute('hidden', '');
   panel.append(element);
+  const verticalTabs = panel.querySelector('.vertical-tabs');
+  verticalTabs.append(li({
+    role: 'tab',
+    ariaSelected: verticalTabs.children.length === 0,
+    ariaControls: `panel-${modelKey}-${horsepower}`,
+    id: `tab-${modelKey}-${horsepower}`,
+    tabindex: verticalTabs.children.length,
+  }, horsepower));
 }
