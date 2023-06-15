7ddd699f574b099040ef6cc8f1e5ff3bb6886235
Aravindo Wingeier <wingeier@adobe.com>
2023-06-15 10:12:39 -0700

    cleanup

--- blocks/performance-data/performance-data.js
+++ blocks/performance-data/performance-data.js
@@ -6,40 +6,5 @@
  * @return {Promise<void>}
  */
 export default async function decorate(block) {
-
-  // const keyFacts = domEl('dl', { class: 'key-specs' });
-  // facts.forEach((row) => {
-  //   keyFacts.appendChild(domEl('dt', row[0]));
-  //   keyFacts.appendChild(domEl('dd', row[1]));
-  // });
-  //
-  // block.innerText = '';
-  // block.append(keyFacts);
-  //
-  // const diagram = div({ class: 'performance-chart' });
-  // diagram.append(div({ class: 'loading-spinner' }));
-  // block.append(diagram);
-  //
-  // // drawing the chart is delayed until the tab is visible. this avoids drawing charts
-  // // for tabs that are not visible.
-  // const observer = new IntersectionObserver(((entries) => {
-  //   entries.forEach((entry) => {
-  //     if (entry.isIntersecting) {
-  //       drawChart(diagram, performanceData);
-  //     }
-  //   });
-  // }));
-  // observer.observe(block);
-  //
-
-  // addPerformanceData(block.closest('.performance-specifications-container').querySelector('.performance-specifications'),
-  //   facts, performanceData, categoryKey, horsepower);
-
-  const parent = block.parentElement;
-  parent.remove();
+  block.parentElement.remove();
 }
-
-
-
-
-
