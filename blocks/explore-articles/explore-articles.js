d1a38376d311ffafd75f631673e0da2f262e8f0b
SantiagoHomps-NC <santiago.homps@netcentric.biz>
2023-06-16 16:32:31 +0200

    done but linter issues

--- blocks/explore-articles/explore-articles.js
+++ blocks/explore-articles/explore-articles.js
@@ -2,12 +2,25 @@ import { createElement, getTextLabel } from '../../scripts/scripts.js';
 import { getAllArticles } from '../recent-articles/recent-articles.js';
 
 const allArticles = await getAllArticles();
-// let shownArticles = allArticles.slice(0, 8)
-
 const allCategories = [...new Set(allArticles.map((article) => article.category))];
 const allTrucks = [...new Set(allArticles.map((article) => article.truck))];
 
 const [categoryPlaceholder, truckPlaceholder] = getTextLabel('Article filter placeholder').split(',');
+
+const articlesPerChunk = 4;
+let counter = 1;
+
+const divideArray = (mainArray, perChunk) => {
+  const dividedArrays = mainArray.reduce((resultArray, item, index) => { 
+    const chunkIndex = Math.floor(index/perChunk);
+    if(!resultArray[chunkIndex]) {
+      resultArray[chunkIndex] = [];
+    }
+    resultArray[chunkIndex].push(item)
+    return resultArray
+  }, [])
+  return dividedArrays
+}
 
 const getOptions = (list, placeholder) => {
   const options = [];
@@ -71,66 +84,98 @@ const buildArticle = (e) => {
   return article;
 };
 
-const buildArticleList = (articles) => {
-  const perChunk = 8;  
+const loadMoreArticles = (evt, articleGroups, totalAmount, amountOfGroups) => {
+  evt.preventDefault();
+  const activeButton = evt.srcElement
+  const allShownArticles = document.querySelectorAll('.article');
+  const lastShownArticle = allShownArticles[allShownArticles.length - 1]
+  const lastShownId = +(lastShownArticle.id.split('-').pop())
+  const nextArticleGroup = articleGroups[lastShownId + 1]
 
-
-  const groupedArticles = articles.reduce((resultArray, item, index) => { 
-    const chunkIndex = Math.floor(index/perChunk);
-    if(!resultArray[chunkIndex]) {
-      resultArray[chunkIndex] = [];
-    }
-    resultArray[chunkIndex].push(item)
-    return resultArray
-  }, [])
-
-  
-  const articlesSection = createElement('div', 'explore-articles-articles');
-  const paginationSection = createElement('div', 'explore-articles-pagination');
-
-  const list = createElement('div', 'list');
-
-  const amountOfArticles = createElement('p', 'article-pagination');
-  amountOfArticles.textContent = `${articles.length} articles`;
-
-  const pages = createElement('p', 'article-pagination');
-  pages.textContent = `Page X of ${groupedArticles.length}`;
-
-  const emptyMessage = createElement('p', 'article-empty-message');
-  emptyMessage.textContent = getTextLabel('No article Message');
-
-  if (articles.length === 0) {
-    articlesSection.append(emptyMessage);
-  } else {
-    articlesSection.append(amountOfArticles, pages);
-  }
-
-
-
-
-
-
-
-
-console.log(groupedArticles)
-console.log(groupedArticles.length)
-
-
-
-
-
-
-groupedArticles[0].forEach((e) => {
-    const article = buildArticle(e);
-    list.append(article);
+  nextArticleGroup.forEach((e, idx) => {
+    if (idx != 0) lastShownArticle.insertAdjacentElement('afterend', e)
   });
 
-  articlesSection.append(list);
+  console.warn(amountOfGroups)
+  console.warn(counter)
+  
+  if ((amountOfGroups - 1) <= counter) {
+    console.log(amountOfGroups)
+    console.log(counter)
+    activeButton.remove()
+  }
+  
+  counter = counter + 1;
+
+  // console.log(allShownArticles.length, totalAmount)
+  // if (allShownArticles.length <= totalAmount) activeButton.remove()
+  // if (allShownArticles.length !<= totalAmount) activeButton.remove()
+}
+
+const addAllArrays = (array) => {
+  let initialValue = 0
+  const totalArticles = array.reduce(
+    (acc, curr) => acc + curr.length,
+    initialValue
+  );
+  return totalArticles
+}
+
+const getArticleGroups = (artGroup) => {
+  const groups = []
+    artGroup.forEach((articleGroup, idx) => {
+      const group = [idx]
+      articleGroup.forEach((el) => {
+        const article = buildArticle(el);
+        article.id = `group-${idx}`
+        group.push(article)
+      });
+      groups.push(group)
+    });
+  return groups
+}
+
+const buildFirstArticles = (art, section) => {
+  const firstArticles = art[0]
+  firstArticles.forEach((e, idx) => {
+    if (idx != 0) section.append(e)
+  });
+}
+
+const buildArticleList = (articles) => {
+  const groupedArticles = divideArray(articles, articlesPerChunk)
+  const articleGroups = getArticleGroups(groupedArticles)
+  const totalArticlesNumber = addAllArrays(groupedArticles)
+  const amountOfGroups = articleGroups.length
+
+  const paginationSection = createElement('div', 'pagination-section');
+  const articlesSection = createElement('div', 'explore-articles-articles');
+
+  const amountOfArticles = createElement('p', 'article-amount');
+  amountOfArticles.textContent = (totalArticlesNumber != 0) ?  `${totalArticlesNumber} articles` : getTextLabel('No article Message')
+
+  paginationSection.append(amountOfArticles)
+  articlesSection.append(paginationSection);
+
+  const moreSection = createElement('div', 'explore-articles-more');
+  const moreButton = createElement('button', 'more-btn');
+  moreButton.textContent = getTextLabel('Load more articles button');
+  moreButton.addEventListener('click', (evt) => loadMoreArticles(evt, articleGroups, totalArticlesNumber, amountOfGroups))
+  if (totalArticlesNumber > articlesPerChunk) moreSection.append(moreButton);
+
+  if (articleGroups.length != 0) {
+    const articleListSection = createElement('div', 'article-list');
+    buildFirstArticles(articleGroups, articleListSection);
+    articlesSection.append(articleListSection, moreSection);
+  }
   return articlesSection;
 };
 
 const handleForm = (event) => {
-  const fieldset = event.srcElement.parentNode.parentNode;
+  // const fieldset = event.srcElement.parentNode.parentNode;
+  event.preventDefault();
+  counter = 1;
+  const fieldset = document.querySelector('#explore-magazine-fieldset');
   const selects = fieldset.querySelectorAll('select');
   const [category, truck] = selects;
 
@@ -152,7 +197,9 @@ const handleForm = (event) => {
   const articleList = document.querySelector('.explore-articles-articles');
 
   articleList.textContent = '';
-  const filteredArticles = buildArticleList(filteredList);
+  //add grouped articles
+  const filteredArticles = buildArticleList(filteredList, 0);
+  // if (filteredList.length != 0) articleList.append(filteredArticles);
   articleList.append(filteredArticles);
 };
 
@@ -161,7 +208,7 @@ const buildFieldset = () => {
   const form = createElement('form', ['form', 'filter-list'], { method: 'get', name: 'article-fieldset' });
   form.addEventListener('change', handleForm);
 
-  const fieldset = createElement('fieldset', ['fieldset', 'filter-list'], { method: 'get', name: 'article-fieldset' });
+  const fieldset = createElement('fieldset', ['fieldset', 'filter-list'], { method: 'get', name: 'article-fieldset', id: 'explore-magazine-fieldset' });
 
   const categoryField = createElement('div', 'category-field');
   const trucksField = createElement('div', 'trucks-field');
@@ -176,6 +223,7 @@ const buildFieldset = () => {
 
   return formSection;
 };
+
 
 export default async function decorate(block) {
   const children = block.querySelectorAll('p');
@@ -192,7 +240,8 @@ export default async function decorate(block) {
 
   headingSection.append(h4Element, text);
   contentSection.append(buildFieldset());
-  contentSection.append(buildArticleList(allArticles));
+
+  contentSection.append(buildArticleList(allArticles, 0));
 
   generalSection.append(headingSection, contentSection);
 
