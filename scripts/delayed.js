b9df32a7dc0ea87560295f6990a0d9a07e6497a6
Aravindo Wingeier <wingeier@adobe.com>
2023-06-15 17:34:36 -0700

    cleanup

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,16 +1,13 @@
 // eslint-disable-next-line import/no-cycle
-import { loadScript, sampleRUM } from './lib-franklin.js';
+import { sampleRUM, loadScript } from './lib-franklin.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
 
-if (!window.location.host.includes('hlx.page') && !window.location.host.includes('localhost')) {
-  // OneTrust Cookies Consent Notice
-  // loadScript('https://cdn.cookielaw.org/scripttemplates/otSDKStub.js', {
-  //   type: 'text/javascript',
-  //   charset: 'UTF-8',
-  //   'data-domain-script': 'bf50d0a6-e209-4fd4-ad2c-17da5f9e66a5',
-  // });
-}
+loadScript('https://cdn.cookielaw.org/scripttemplates/otSDKStub.js', {
+  type: 'text/javascript',
+  charset: 'UTF-8',
+  'data-domain-script': 'bf50d0a6-e209-4fd4-ad2c-17da5f9e66a5',
+});
