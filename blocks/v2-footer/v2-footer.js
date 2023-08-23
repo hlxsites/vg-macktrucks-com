3047a7215bfbbf51718aaf689aafbb79d0051e47
Laura Jove <laura.jove@netcentric.biz>
2023-08-23 17:27:42 +0200

    footer

---
+++ blocks/v2-footer/v2-footer.js
@@ -0,0 +1,213 @@
+import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
+import { createElement } from '../../scripts/scripts.js';
+
+const addClassToTitle = (block, className) => {
+  const headings = block.querySelectorAll('h1, h2, h3, h4, h5, h6');
+  [...headings].forEach((h) => h.classList.add(className));
+};
+
+const blockName = 'v2-footer';
+const blockNamePrefooter = 'v2-prefooter';
+const blockNameTruckList = 'v2-footer-truck-list';
+const blockNameMenu = 'v2-footer-menu';
+const blockNameLegal = 'v2-footer-legal';
+
+function displayScrollToTop(buttonEl) {
+  const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+  buttonEl.style.display = scrollTop > 160 ? 'block' : 'none';
+}
+
+function goToTopFunction() {
+  const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+  let timeOut;
+
+  if (scrollTop !== 0) {
+    window.scrollBy(0, -50);
+    timeOut = setTimeout(goToTopFunction, 10);
+    return;
+  }
+
+  clearTimeout(timeOut);
+}
+
+function addScrollToTopButton(mainEl) {
+  const scrollToTopButton = createElement('button', ['scroll-to-top', 'button'], { title: 'Go to the top of the page' });
+  const icon = createElement('i', ['fa', 'fa-angle-up']);
+  scrollToTopButton.appendChild(icon);
+
+  scrollToTopButton.addEventListener('click', goToTopFunction);
+  window.addEventListener('scroll', () => displayScrollToTop(scrollToTopButton));
+  mainEl.append(scrollToTopButton);
+}
+
+function findList(ele) {
+  if (ele.classList.contains(blockNameTruckList)) {
+    return ele;
+  }
+  return findList(ele.parentElement);
+}
+
+function toggleExpand(targetH3) {
+  const clickedColumn = findList(targetH3);
+  const isExpanded = clickedColumn.classList.contains('expand');
+  const wrapper = targetH3.closest(`.${blockNameTruckList}`);
+  const content = wrapper.querySelector(`.${blockNameTruckList}__items`);
+  if (wrapper === clickedColumn && !isExpanded) {
+    wrapper.classList.add('expand');
+    content.style.maxHeight = `${content.scrollHeight}px`;
+  } else {
+    wrapper.classList.remove('expand');
+    content.style.maxHeight = null;
+  }
+}
+
+export default async function decorate(block) {
+  const cfg = readBlockConfig(block);
+  block.textContent = '';
+
+  const footerPath = cfg.footer || '/footer';
+  const resp = await fetch(`${footerPath}.plain.html`);
+  const html = await resp.text();
+
+  block.innerHTML = html;
+
+  // Eloqua form necessary variables
+  const eloquaForm = block.querySelector('.eloqua-form');
+  let observer = null;
+  let submitButtonFixed = false;
+  let checkboxFixed = false;
+
+  const footerItems = block.querySelectorAll(`:scope > div .${blockName} > div`);
+
+  const prefooter = footerItems?.[0].querySelector(':scope > div');
+  const truckList = footerItems?.[1].querySelector(':scope > div');
+  const footerMenu = footerItems?.[2];
+  const footerLegal = footerItems?.[3].querySelector(':scope > div');
+
+  const newFooter = createElement('div');
+
+  // Prefooter
+  if (prefooter) {
+    prefooter.classList.add(blockNamePrefooter);
+    newFooter.append(prefooter);
+  }
+
+  // Truck list
+  if (truckList) {
+    truckList.classList.add(blockNameTruckList);
+    truckList.querySelector('ul')?.classList.add(`${blockNameTruckList}__items`);
+    addClassToTitle(truckList, `${blockNameTruckList}__title`);
+
+    newFooter.append(truckList);
+  }
+
+  // Menu: social media + logo + menu list + newsletter form
+  if (footerMenu) {
+    const newMenu = createElement('div', blockNameMenu);
+
+    // Logo
+    const logo = footerMenu.querySelector('picture');
+    logo.classList.add(`${blockNameMenu}__logo`);
+    newMenu.appendChild(logo);
+
+    // Social media
+    const socialMedia = footerMenu.querySelector(':scope > div ul');
+    socialMedia.classList.add(`${blockNameMenu}__socialmedia`);
+    const socialLinks = socialMedia.querySelectorAll('a');
+    [...socialLinks].forEach((a) => { a.target = '_blank'; });
+    newMenu.appendChild(socialMedia);
+
+    // remove div which contained logo and social media
+    footerMenu.firstElementChild.remove();
+
+    // Menu Columns: menu
+    footerMenu.classList.add(`${blockNameMenu}__columns`);
+    addClassToTitle(footerMenu, `${blockNameMenu}__title`);
+    const menuList = footerMenu.querySelectorAll(':scope > div');
+    menuList.forEach((item) => item.classList.add(`${blockNameMenu}__column`));
+
+    // Menu Columns: Newsletter form
+    const lastColumn = footerMenu.querySelector(':scope > div:last-child');
+    lastColumn.append(eloquaForm);
+
+    newMenu.appendChild(footerMenu);
+
+    newFooter.append(newMenu);
+  }
+
+  if (footerLegal) {
+    footerLegal.classList.add(blockNameLegal);
+
+    newFooter.append(footerLegal);
+  }
+
+  block.innerHTML = newFooter.innerHTML;
+
+  await decorateIcons(block);
+
+  const onFormLoaded = (mutationList) => {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const mutation of mutationList) {
+      if (submitButtonFixed && checkboxFixed) {
+        observer.disconnect();
+        return;
+      }
+
+      if (mutation.type === 'childList') {
+        const submitButton = block.querySelector('input[type="submit"]');
+        const emailInput = block.querySelector('input[name="emailAddress"]');
+        const label = emailInput.parentElement.querySelector('label');
+        const emailAndSubmitContainer = createElement('span', ['email-and-submit-container']);
+
+        // change the submit button to arrow button
+        // and display it sticked to the right side of email input
+        if (submitButton && emailInput) {
+          const parent = emailInput.parentElement;
+          submitButton.value = '';
+          emailAndSubmitContainer.append(emailInput, submitButton);
+          parent.append(emailAndSubmitContainer);
+
+          if (label) {
+            emailInput.setAttribute('placeholder', label.innerText.replace('*', '').trim());
+            label.remove();
+          }
+
+          submitButtonFixed = true;
+        }
+
+        const checkbox = block.querySelector('.checkbox-span input[type="checkbox"]');
+        const checkboxLabel = block.querySelector('.checkbox-span .checkbox-label');
+        // customization of the checkbox
+        if (checkbox && checkboxLabel) {
+          const checkboxId = 'footer-subscribe-checkbox';
+          const checkboxParent = checkbox.parentElement;
+          checkbox.setAttribute('id', checkboxId);
+          checkboxLabel.setAttribute('for', checkboxId);
+          checkboxParent.classList.add('confirm-checkbox');
+
+          if (emailInput) {
+            // showing the checkbox only when the user start typing
+            emailInput.addEventListener('input', () => { checkboxParent.classList.add('show'); }, { once: true });
+          }
+
+          checkboxFixed = true;
+        }
+      }
+    }
+  };
+
+  observer = new MutationObserver(onFormLoaded);
+  observer.observe(eloquaForm, {
+    childList: true,
+    attributes: false,
+    subtree: true,
+  });
+
+  block.addEventListener('click', (e) => {
+    if (e.target.classList.contains(`${blockNameTruckList}__title`)) {
+      toggleExpand(e.target);
+    }
+  });
+
+  addScrollToTopButton(block);
+}
