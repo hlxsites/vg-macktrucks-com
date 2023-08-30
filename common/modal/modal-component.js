39fd6d667771d91fd4be784cc4f892da464afc12
Laura Jove <laura.jove@netcentric.biz>
2023-08-30 06:57:30 +0200

    update fill & stroke functionallity in svgs

--- common/modal/modal-component.js
+++ common/modal/modal-component.js
@@ -39,8 +39,8 @@ const createModal = () => {
   const closeIcon = createElement('span', ['icon', 'icon-close']);
   const svgCloseIcon = document.createRange().createContextualFragment(`
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path id="Line 103" d="M5.00195 5L19.1441 19.1421" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
-      <path id="Line 104" d="M19.1426 5L5.00044 19.1421" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+      <path d="M5.00195 5L19.1441 19.1421" stroke="var(--color-icon, #000)" stroke-width="2" stroke-linecap="round"/>
+      <path d="M19.1426 5L5.00044 19.1421" stroke="var(--color-icon, #000)" stroke-width="2" stroke-linecap="round"/>
     </svg>
   `);
   closeIcon.append(...svgCloseIcon.children);
