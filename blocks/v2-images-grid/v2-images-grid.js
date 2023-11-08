071f5070bfe7b9f77813694bde11c549c78cdde2
Tomasz Dziezyk <tomasz.dziezyk@ds.pl>
2023-11-08 08:40:16 +0100

    #38 Create modal content

--- blocks/v2-images-grid/v2-images-grid.js
+++ blocks/v2-images-grid/v2-images-grid.js
@@ -1,8 +1,103 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
-import { createElement, removeEmptyTags, getTextLabel } from '../../scripts/common.js';
+import {
+  createElement, removeEmptyTags, getTextLabel, debounce,
+} from '../../scripts/common.js';
 import { getAllElWithChildren } from '../../scripts/scripts.js';
 
 const blockClassName = 'v2-images-grid';
+
+const createModalContent = (content) => {
+  const carouselItemsList = createElement('ul', { classes: `${blockClassName}__carousel-items-list` });
+  const carouselImagesList = createElement('ul', { classes: `${blockClassName}__carousel-preview-list` });
+
+  const debouncedOnItemChange = debounce((index) => {
+    carouselImagesList.scrollTo({
+      left: index * 90,
+      behavior: 'smooth',
+    });
+  }, 100);
+
+  [...content.querySelectorAll('.v2-images-grid__item')].forEach((el, index) => {
+    const image = el.querySelector('img');
+
+    // adding image to carousel preview
+    const carouselImage = createOptimizedPicture(image.src, image.alt, false, [{ width: '80' }]);
+    const carousePreviewlItem = createElement('li', { classes: `${blockClassName}__carousel-preview-item` });
+    const buttonWithImage = createElement('button');
+
+    buttonWithImage.addEventListener('click', () => {
+      const itemWidth = carouselItemsList.getBoundingClientRect().width;
+
+      carouselItemsList.scrollTo({
+        left: index * itemWidth,
+        behavior: 'smooth',
+      });
+    });
+
+    buttonWithImage.append(carouselImage);
+    carousePreviewlItem.append(buttonWithImage);
+    carouselImagesList.append(carousePreviewlItem);
+
+    // creating item content
+    const itemImage = createOptimizedPicture(image.src, image.alt, false);
+    const [, caption, description] = el.querySelectorAll('p');
+    const carouselItem = createElement('li', { classes: `${blockClassName}__carousel-item` });
+
+    carouselItem.append(itemImage, description || caption);
+    carouselItemsList.append(carouselItem);
+
+    const options = {
+      root: carouselItemsList,
+      rootMargin: '0px',
+      threshold: 1.0,
+    };
+
+    const observer = new IntersectionObserver(() => {
+      debouncedOnItemChange(index);
+    }, options);
+
+    observer.observe(carouselItem);
+  });
+
+  const wrapper = createElement('div', { classes: `${blockClassName}__modal-content` });
+
+  wrapper.innerHTML = `
+    <div>
+      <button><-</button>
+      <button>-></button>
+    </div>
+  `;
+
+  [...wrapper.querySelectorAll('button')].forEach((el, elIndex) => {
+    const modifiers = [-1, 1];
+
+    el.addEventListener('click', () => {
+      const itemWidth = carouselItemsList.getBoundingClientRect().width;
+      let index = Math.round(carouselItemsList.scrollLeft / itemWidth) + modifiers[elIndex];
+
+      if (index < 0) {
+        index = carouselItemsList.children.length - 1;
+      }
+
+      if (index > carouselItemsList.children.length - 1) {
+        index = 0;
+      }
+
+      carouselImagesList.scrollTo({
+        left: index * 90,
+        behavior: 'smooth',
+      });
+
+      carouselItemsList.scrollTo({
+        left: index * itemWidth,
+        behavior: 'smooth',
+      });
+    });
+  });
+  wrapper.append(carouselItemsList, carouselImagesList);
+  document.querySelector('.v2-images-grid.block').append(wrapper);
+};
+
 export default function decorate(block) {
   // all items are inside a ul list with classname called 'v2-images-grid__items'
   const ul = createElement('ul', { classes: `${blockClassName}__items` });
@@ -16,6 +111,8 @@ export default function decorate(block) {
     cell.remove();
   });
   block.append(ul);
+
+  createModalContent(ul);
 
   // give format to the first 4 list items
   [...ul.children].forEach((li, idx) => {
