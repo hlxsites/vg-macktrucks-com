25bc37697379eb08860c0758da33c8a6db2fbf0f
Tomasz Dziezyk <tomasz.dziezyk@ds.pl>
2023-11-09 13:37:56 +0100

    #43 Show modal on Open gallery button click

--- blocks/v2-images-grid/v2-images-grid.js
+++ blocks/v2-images-grid/v2-images-grid.js
@@ -3,6 +3,7 @@ import {
   createElement, removeEmptyTags, getTextLabel, debounce,
 } from '../../scripts/common.js';
 import { getAllElWithChildren } from '../../scripts/scripts.js';
+import { showModal } from '../../common/modal/modal-component.js';
 
 const blockClassName = 'v2-images-grid';
 
@@ -59,14 +60,24 @@ const createModalContent = (content) => {
     observer.observe(carouselItem);
   });
 
+  const itemsWrapper = createElement('div', { classes: `${blockClassName}__carousel-items-wrapper` });
   const wrapper = createElement('div', { classes: `${blockClassName}__modal-content` });
 
-  wrapper.innerHTML = `
-    <div>
-      <button><-</button>
-      <button>-></button>
+  itemsWrapper.innerHTML = `
+    <div class="${blockClassName}__modal-arrows-wrapper">
+      <button class="${blockClassName}__modal-arrow">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+          <path fill="var(--color-icon, #000)" d="M21 11C21.5523 11 22 11.4477 22 12C22 12.5523 21.5523 13 21 13V11ZM2.29289 12.7071C1.90237 12.3166 1.90237 11.6834 2.29289 11.2929L8.65685 4.92893C9.04738 4.53841 9.68054 4.53841 10.0711 4.92893C10.4616 5.31946 10.4616 5.95262 10.0711 6.34315L4.41421 12L10.0711 17.6569C10.4616 18.0474 10.4616 18.6805 10.0711 19.0711C9.68054 19.4616 9.04738 19.4616 8.65685 19.0711L2.29289 12.7071ZM21 13L3 13V11L21 11V13Z" />
+        </svg>
+      </button>
+      <button class="${blockClassName}__modal-arrow">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill="var(--color-icon, #000)" d="M3 11C2.44772 11 2 11.4477 2 12C2 12.5523 2.44772 13 3 13L3 11ZM21.7071 12.7071C22.0976 12.3166 22.0976 11.6834 21.7071 11.2929L15.3431 4.92893C14.9526 4.53841 14.3195 4.53841 13.9289 4.92893C13.5384 5.31946 13.5384 5.95262 13.9289 6.34315L19.5858 12L13.9289 17.6569C13.5384 18.0474 13.5384 18.6805 13.9289 19.0711C14.3195 19.4616 14.9526 19.4616 15.3431 19.0711L21.7071 12.7071ZM3 13L21 13V11L3 11L3 13Z" />
+        </svg>
+      </button>
     </div>
   `;
+  itemsWrapper.append(carouselItemsList);
 
   [...wrapper.querySelectorAll('button')].forEach((el, elIndex) => {
     const modifiers = [-1, 1];
@@ -94,8 +105,10 @@ const createModalContent = (content) => {
       });
     });
   });
-  wrapper.append(carouselItemsList, carouselImagesList);
-  document.querySelector('.v2-images-grid.block').append(wrapper);
+
+  wrapper.append(itemsWrapper, carouselImagesList);
+
+  return wrapper;
 };
 
 export default function decorate(block) {
@@ -112,7 +125,7 @@ export default function decorate(block) {
   });
   block.append(ul);
 
-  createModalContent(ul);
+  const modalContent = createModalContent(ul);
 
   // give format to the first 4 list items
   [...ul.children].forEach((li, idx) => {
@@ -145,6 +158,10 @@ export default function decorate(block) {
     classes: ['button', 'button--large', 'button--primary'],
   });
   button.textContent = getTextLabel('Open Gallery');
+  button.addEventListener('click', () => {
+    showModal(modalContent);
+  });
+
   block.append(button);
 
   // remove empty tags
