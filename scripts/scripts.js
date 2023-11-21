1a70d1c281da26d496a5a132d70654df10288055
Jonatan Lledo <117984204+jonatan-lledo-netcentric@users.noreply.github.com>
2023-11-21 15:34:41 +0100

    Update URLs to -cl or .cl

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -269,8 +269,8 @@ export function decorateLinks(block) {
       }
 
       const url = new URL(link.href);
-      const external = !url.host.match('macktrucks.com') && !url.host.match('.hlx.(page|live)') && !url.host.match('localhost');
-      if (url.host.match('build.macktrucks.com') || url.pathname.endsWith('.pdf') || external) {
+      const external = !url.host.match('macktrucks.cl') && !url.host.match('.hlx.(page|live)') && !url.host.match('localhost');
+      if (url.host.match('build.macktrucks.cl') || url.pathname.endsWith('.pdf') || external) {
         link.target = '_blank';
       }
     });
