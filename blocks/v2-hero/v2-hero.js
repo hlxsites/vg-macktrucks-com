cafd4d9518ac131d388b96658a36f87a07264bdb
Syb Wartna <sybren.wartna@cognizant.com>
2023-11-23 21:39:49 +0100

    refactor naming structure

--- blocks/v2-hero/v2-hero.js
+++ blocks/v2-hero/v2-hero.js
@@ -1,30 +1,30 @@
 export default async function decorate(block) {
-  const heroClass = 'v2-hero';
+  const blockName = 'v2-hero';
   const picture = block.querySelector('picture');
   const img = picture.querySelector('img');
-  img.classList.add(`${heroClass}__image`);
+  img.classList.add(`${blockName}__image`);
 
   if (picture.parentElement.tagName === 'P') {
     picture.parentElement.remove();
   }
 
   const contentWrapper = block.querySelector(':scope > div');
-  contentWrapper.classList.add(`${heroClass}__content-wrapper`);
+  contentWrapper.classList.add(`${blockName}__content-wrapper`);
 
   const content = block.querySelector(':scope > div > div');
-  content.classList.add(`${heroClass}__content`);
+  content.classList.add(`${blockName}__content`);
 
   const headings = [...content.querySelectorAll('h1, h2, h3, h4, h5, h6')];
-  headings.forEach((h) => h.classList.add(`${heroClass}__heading`));
+  headings.forEach((h) => h.classList.add(`${blockName}__heading`));
 
   const firstHeading = headings[0];
   firstHeading.classList.add('with-marker');
 
   const ctaButtons = content.querySelectorAll('.button-container > a');
   [...ctaButtons].forEach((b) => {
-    b.classList.add(`${heroClass}__cta`, 'button--cta');
+    b.classList.add(`${blockName}__cta`, 'button--cta');
     b.classList.remove('button--primary');
-    b.parentElement.classList.add(`${heroClass}__cta-wrapper`);
+    b.parentElement.classList.add(`${blockName}__cta-wrapper`);
   });
 
   block.prepend(picture);
