0b190a400cd1e75648af361a1c7297be03865182
SantiagoHomps-NC <103571932+SantiagoHomps-NC@users.noreply.github.com>
2023-11-30 12:55:47 -0300

    Icon cards block (#181)
    * #509 Fix displaying header on dealer page
    
    * fix done
    
    * one trust position ix
    
    * Embed block match NZ one
    
    * Update feed.xml
    
    * Fix createElement()  v1-Cards
    
    * Update feed.xml
    
    * Update release.yml
    
    * columns block adapted
    
    * icon cards working
    
    * css clean
    
    * adjustements
    
    * last changes
    
    * specific class
    
    * btn state styling
    
    * comments corrected
    
    * secondary btn margin corrected
    
    * secondary btn class added
    
    * cleanup
    
    ---------
    
    Co-authored-by: Tomasz Dziezyk <tomasz.dziezyk@netcentric.biz>
    Co-authored-by: TomaszDziezykNetcentric <125962117+TomaszDziezykNetcentric@users.noreply.github.com>
    Co-authored-by: Jonatan Lledo <117984204+jonatan-lledo-netcentric@users.noreply.github.com>
    Co-authored-by: aem-code-sync[bot] <aem-code-sync[bot]@users.noreply.github.com>
    Co-authored-by: Syb <133873665+cogniSyb@users.noreply.github.com>

--- blocks/v2-columns/v2-columns.js
+++ blocks/v2-columns/v2-columns.js
@@ -1,9 +1,7 @@
-import { createElement, variantsClassesToBEM } from '../../scripts/common.js';
+import { createElement } from '../../scripts/common.js';
 
 export default async function decorate(block) {
   const blockName = 'v2-columns';
-  const variantClasses = ['info'];
-  variantsClassesToBEM(block.classList, variantClasses, blockName);
 
   const rows = [...block.querySelectorAll(':scope > div')];
   const columns = [...block.querySelectorAll(':scope > div > div')];
@@ -64,35 +62,4 @@ export default async function decorate(block) {
       prevEl.replaceWith(pretitle);
     }
   });
-
-  // logic for info variant
-  if (block.classList.contains(`${blockName}--info`)) {
-    const headings = [...block.querySelectorAll('h3, h4, h5, h6')];
-    const h2List = [...block.querySelectorAll('h2')];
-
-    headings.forEach((h) => {
-      h.classList.add('h5');
-      h.classList.remove('h2');
-    });
-
-    h2List.forEach((h) => {
-      h.classList.add('with-marker', 'h2');
-      h.classList.remove('h1');
-      h.closest(`.${blockName}__column`)?.classList.add(`${blockName}__column--info-main`);
-    });
-
-    // replacing headings (h3, h4, h5, h6) with strong so the block will not break semantic
-    // (example breaking semantic: col 1 -> h5, col 2 -> h2)
-    headings.forEach((heading) => {
-      const newHeadingEl = createElement('strong', { classes: [...heading.classList] });
-      newHeadingEl.innerHTML = heading.innerHTML;
-      heading.replaceWith(newHeadingEl);
-    });
-
-    const buttons = [...block.querySelectorAll('.button-container a')];
-    buttons.forEach((button) => {
-      button.classList.add('standalone-link', `${blockName}__button`);
-      button.classList.remove('button', 'button--primary', 'button--large');
-    });
-  }
 }
