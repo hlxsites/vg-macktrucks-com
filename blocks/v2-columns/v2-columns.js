cd88cf98cc483b8afdbd177fc1907b0b87050538
SantiagoHomps-NC <103571932+SantiagoHomps-NC@users.noreply.github.com>
2024-01-17 15:07:35 +0100

    V2 Columns Block variation #133 (#186)
    ---------
    
    Co-authored-by: aem-code-sync[bot] <aem-code-sync[bot]@users.noreply.github.com>
    Co-authored-by: Lakshmishri <lakshmi.shri.v.a@gmail.com>
    Co-authored-by: Syb Wartna <sybren.wartna@cognizant.com>

--- blocks/v2-columns/v2-columns.js
+++ blocks/v2-columns/v2-columns.js
@@ -1,7 +1,14 @@
-import { createElement } from '../../scripts/common.js';
+import { variantsClassesToBEM, createElement } from '../../scripts/common.js';
 
 export default async function decorate(block) {
+  const blockParent = block.parentElement.parentElement;
   const blockName = 'v2-columns';
+
+  const variantClasses = ['with-background-image'];
+  variantsClassesToBEM(block.classList, variantClasses, blockName);
+
+  const isBackgroundImageVariant = block.classList.contains(`${blockName}--with-background-image`);
+  const hasHeader = blockParent.classList.contains('header-with-mark');
 
   const rows = [...block.querySelectorAll(':scope > div')];
   const columns = [...block.querySelectorAll(':scope > div > div')];
@@ -14,7 +21,7 @@ export default async function decorate(block) {
     col.classList.add(`${blockName}__column`);
 
     const picture = col.querySelector('picture');
-    const allTextElmts = col.querySelectorAll('p');
+    const allTextElmts = col.querySelectorAll('p, ul, ol');
     const bodyElmts = [];
 
     if (picture) {
@@ -33,14 +40,35 @@ export default async function decorate(block) {
     });
     bodyElmts.forEach((e) => e.classList.add(`${blockName}__body`));
 
-    const buttons = [...col.querySelectorAll('.button-container a')];
-    buttons.forEach((btn) => {
-      btn.classList.add('button', 'button--large', 'button--primary');
-
-      if (btn.parentElement.classList.contains('button-container')) {
-        btn.parentElement.replaceWith(btn);
-      }
+    block.querySelectorAll(`ul.${blockName}__body li`).forEach((item) => {
+      item.classList.add('li--hyphen');
     });
+    const buttons = [...col.querySelectorAll('.button-container a')];
+
+    if (isBackgroundImageVariant) {
+      blockParent.classList.add(`${blockName}-container--with-background-image`);
+      const btnSection = createElement('div', { classes: 'button-container' });
+
+      buttons.forEach((btn) => {
+        btn.classList.add('button--large');
+        const btnContainer = btn.closest('.button-container');
+        btnContainer.replaceWith(btn);
+        btnSection.append(btn);
+      });
+      if (!picture) col.append(btnSection);
+
+      if (hasHeader) {
+        const defaultContent = blockParent.querySelector('.default-content-wrapper');
+        const header = [...defaultContent.querySelectorAll('h1, h2, h3, h4, h5, h6')];
+        header[0].classList.add(`${blockName}__body-header`, 'with-marker');
+        bodyElmts[0].insertAdjacentElement('beforebegin', header[0]);
+        defaultContent.remove();
+      }
+    } else {
+      buttons.forEach((btn) => {
+        btn.classList.add('button--large');
+      });
+    }
 
     const headings = [...col.querySelectorAll('h1, h2, h3, h4, h5, h6')];
     headings.forEach((heading) => heading.classList.add(`${blockName}__heading`, 'h2'));
