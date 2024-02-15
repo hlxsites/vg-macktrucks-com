9d99c39c4423acf049f00e944fb0b07f1cedc036
SantiagoHomps-NC <santiago.homps@netcentric.biz>
2024-02-15 12:59:51 +0100

    first approach

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -68,3 +68,17 @@ async function loadHotjar() {
   })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
   /* eslint-enable */
 }
+
+// Account Engagement Tracking Code
+const loadAccountEngagementTracking = () => {
+  const body = document.querySelector('body');
+  const script = document.createElement('script');
+  script.type = 'text/javascript';
+
+  script.text = "piAId = '1039343'; piCId = '122594'; piHostname = 'pi.pardot.com'; (function() { function async_load(){ var s = document.createElement('script'); s.type = 'text/javascript'; s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js'; var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c); } if(window.attachEvent) { window.attachEvent('onload', async_load); } else { window.addEventListener('load', async_load, false); } })();";
+
+  body.append(script);
+};
+
+// Still need to check if some cookie consent should be verified before
+loadAccountEngagementTracking();
