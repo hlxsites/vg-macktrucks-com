e093f5f130fb1d71110b07451b45f201d055a122
Syb Wartna <sybren.wartna@cognizant.com>
2024-02-28 12:54:04 +0100

    refactor passing data to iframe

--- blocks/v2-iframe/v2-iframe.js
+++ blocks/v2-iframe/v2-iframe.js
@@ -1,20 +1,56 @@
 import { createElement, variantsClassesToBEM } from '../../scripts/common.js';
 
+function removeDuplicateParams(queryString) {
+  const params = new URLSearchParams(queryString);
+  const uniqueParams = new URLSearchParams();
+
+  params.forEach((value, key) => {
+    if (!uniqueParams.has(key)) {
+      uniqueParams.append(key, value);
+    }
+  });
+
+  return uniqueParams.toString();
+}
+
 export default async function decorate(block) {
-  const link = block.querySelector('a')?.getAttribute('href') || block.textContent.trim();
   const title = block.querySelector('h1, h2, h3, h4, h5, h6').textContent;
-  const iframe = createElement('iframe', { props: { src: link, frameborder: 0, title } });
+  const passURLs = block.classList.contains('pass-urls');
   const fixedHeightClass = [...block.classList].find((el) => /[0-9]+px/.test(el));
-  const maxWidthClass = [...block.classList].find((el) => /width-[0-9]+px/.test(el));
+  let link = block.querySelector('a')?.getAttribute('href') || block.textContent.trim();
+
+  link += window.location.search ? `#/summary/?${removeDuplicateParams(window.location.search)}` : '';
+  if (!window.location.search && window.location.hash) {
+    link += window.location.hash;
+  }
+
+  const iframe = createElement('iframe', {
+    props: {
+      src: link, frameborder: 0, title, allow: 'clipboard-write',
+    },
+  });
 
   variantsClassesToBEM(block.classList, ['full-viewport'], 'v2-iframe');
 
+  if (passURLs) {
+    const referer = document.referrer;
+    let messageValue;
+
+    if (referer && new URL(referer).origin === window.location.origin) {
+      messageValue = referer;
+    } else {
+      messageValue = window.location.origin;
+    }
+
+    const message = {
+      type: 'URL',
+      value: messageValue,
+    };
+    iframe.contentWindow.postMessage(message, '*');
+  }
+
   if (fixedHeightClass) {
     iframe.height = fixedHeightClass;
-  }
-  if (maxWidthClass) {
-    const maxWidth = maxWidthClass.split('width-')[1];
-    iframe.style.maxWidth = maxWidth;
   }
 
   block.replaceChildren(iframe);
