a43f6bb03d17ae0d476ebec21699be505d7e1009
SantiagoHomps-NC <santiago.homps@netcentric.biz>
2024-03-14 20:51:44 +0100

    carousel missing

--- blocks/v2-hotspots/v2-hotspots.js
+++ blocks/v2-hotspots/v2-hotspots.js
@@ -38,6 +38,8 @@ export default function decorate(block) {
 
   decorateImageAndTitle(block.querySelector('.main'), firstPicture, title);
 
+  decorateFeaturesForMobileView(block);
+
   block.querySelector('.hotspot-layover')
     .addEventListener('click', (event) => {
       const layoverDialog = block.querySelector('.hotspot-layover-box.is-active');
@@ -192,19 +194,6 @@ function addMobileAlternativeCards(hotspot, main) {
             <div class="feature">
                 <h4 class="feature-title"></h4>
                 <picture></picture>
-                <div class="hotspot-mobile-controls">
-                  <a class="hotspot-mobile-button prev">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="18" viewBox="0 0 22 18" fill="none">
-                        <path d="M21.6298 9.03663C21.6298 9.36032 21.4981 9.67075 21.2636 9.89963C21.0292 10.1285 20.7113 10.2571 20.3797 10.2571H5.06695L10.4316 15.495C10.5482 15.6083 10.6406 15.7431 10.7037 15.8914C10.7668 16.0398 10.7993 16.1988 10.7993 16.3595C10.7993 16.5201 10.7668 16.6791 10.7037 16.8275C10.6406 16.9758 10.5482 17.1106 10.4316 17.224C10.1957 17.4513 9.87764 17.5787 9.5462 17.5787C9.21476 17.5787 8.89665 17.4513 8.66077 17.224L1.16063 9.90113C1.04409 9.78774 0.951628 9.65301 0.888537 9.50466C0.825446 9.35631 0.792969 9.19726 0.792969 9.03663C0.792969 8.876 0.825446 8.71695 0.888537 8.56859C0.951628 8.42024 1.04409 8.28551 1.16063 8.17213L8.66077 0.849299C8.8956 0.620019 9.2141 0.491211 9.5462 0.491211C9.71064 0.491211 9.87347 0.522834 10.0254 0.584275C10.1773 0.645716 10.3154 0.735771 10.4316 0.849299C10.5479 0.962827 10.6401 1.0976 10.7031 1.24594C10.766 1.39427 10.7984 1.55325 10.7984 1.7138C10.7984 1.87435 10.766 2.03333 10.7031 2.18166C10.6401 2.33 10.5479 2.46477 10.4316 2.5783L5.06695 7.81616H20.3797C20.7113 7.81616 21.0292 7.94474 21.2636 8.17362C21.4981 8.40251 21.6298 8.71294 21.6298 9.03663Z" fill="#09161F"/>
-                    </svg>
-                  </a>
-                  <span class="hotspot-mobile-pagination"></span>
-                  <a class="hotspot-mobile-button next">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="21" height="18" viewBox="0 0 21 18" fill="none">
-                        <path d="M20.2006 9.90113L12.9348 17.224C12.7063 17.4513 12.3981 17.5787 12.077 17.5787C11.756 17.5787 11.4478 17.4513 11.2193 17.224C11.1064 17.1106 11.0168 16.9758 10.9557 16.8275C10.8946 16.6791 10.8631 16.5201 10.8631 16.3595C10.8631 16.1988 10.8946 16.0398 10.9557 15.8914C11.0168 15.7431 11.1064 15.6083 11.2193 15.495L16.4163 10.2571H1.58205C1.26089 10.2571 0.952875 10.1285 0.725776 9.89963C0.498677 9.67075 0.371094 9.36032 0.371094 9.03663C0.371094 8.71294 0.498677 8.40251 0.725776 8.17362C0.952875 7.94474 1.26089 7.81616 1.58205 7.81616H16.4163L11.2193 2.5783C10.9918 2.34902 10.864 2.03805 10.864 1.7138C10.864 1.55325 10.8954 1.39427 10.9563 1.24594C11.0173 1.0976 11.1066 0.962827 11.2193 0.849299C11.3319 0.735771 11.4656 0.645716 11.6128 0.584275C11.76 0.522834 11.9177 0.491211 12.077 0.491211C12.3988 0.491211 12.7073 0.620019 12.9348 0.849299L20.2006 8.17213C20.3134 8.28551 20.403 8.42024 20.4641 8.56859C20.5253 8.71695 20.5567 8.876 20.5567 9.03663C20.5567 9.19726 20.5253 9.35631 20.4641 9.50466C20.403 9.65301 20.3134 9.78774 20.2006 9.90113Z" fill="#09161F"/>
-                    </svg>
-                  </a>
-              </div>
             </div>
         </div>
     </div>
@@ -216,7 +205,7 @@ function addMobileAlternativeCards(hotspot, main) {
   hotspot.text.forEach(p => {
     const pEl = createElement('p');
     pEl.innerHTML = p.innerHTML;
-    featureBlock.querySelector('.feature .hotspot-mobile-controls').insertAdjacentElement('beforeBegin', pEl);
+    featureBlock.querySelector('.feature').appendChild(pEl);
   });
 
   const mobileDiv = main.querySelector('.hotspot-mobile');
@@ -238,20 +227,28 @@ function updateDesktopLayoverBeforeAndNextButtons(block) {
   });
 }
 
-function updateMobileBeforeAndNextButtons(block) {
-  const dialogs = block.querySelectorAll('.features');
-  dialogs.forEach((dialog, index) => {
-    const prevIndex = index === 0 ? dialogs.length - 1 : index - 1;
-    dialog.querySelector('.hotspot-mobile-button.prev')
-      .addEventListener('click', () => switchToOtherHotspot(block, prevIndex));
+function decorateFeaturesForMobileView(params) {
 
-    const nextIndex = index === dialogs.length - 1 ? 0 : index + 1;
-    dialog.querySelector('.hotspot-mobile-button.next')
-      .addEventListener('click', () => switchToOtherHotspot(block, nextIndex));
+  console.log(params)
+  const paginationStructure = `
+    <div class="hotspot-mobile-controls">
+      <a class="hotspot-mobile-button prev">
+        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="18" viewBox="0 0 22 18" fill="none">
+            <path d="M21.6298 9.03663C21.6298 9.36032 21.4981 9.67075 21.2636 9.89963C21.0292 10.1285 20.7113 10.2571 20.3797 10.2571H5.06695L10.4316 15.495C10.5482 15.6083 10.6406 15.7431 10.7037 15.8914C10.7668 16.0398 10.7993 16.1988 10.7993 16.3595C10.7993 16.5201 10.7668 16.6791 10.7037 16.8275C10.6406 16.9758 10.5482 17.1106 10.4316 17.224C10.1957 17.4513 9.87764 17.5787 9.5462 17.5787C9.21476 17.5787 8.89665 17.4513 8.66077 17.224L1.16063 9.90113C1.04409 9.78774 0.951628 9.65301 0.888537 9.50466C0.825446 9.35631 0.792969 9.19726 0.792969 9.03663C0.792969 8.876 0.825446 8.71695 0.888537 8.56859C0.951628 8.42024 1.04409 8.28551 1.16063 8.17213L8.66077 0.849299C8.8956 0.620019 9.2141 0.491211 9.5462 0.491211C9.71064 0.491211 9.87347 0.522834 10.0254 0.584275C10.1773 0.645716 10.3154 0.735771 10.4316 0.849299C10.5479 0.962827 10.6401 1.0976 10.7031 1.24594C10.766 1.39427 10.7984 1.55325 10.7984 1.7138C10.7984 1.87435 10.766 2.03333 10.7031 2.18166C10.6401 2.33 10.5479 2.46477 10.4316 2.5783L5.06695 7.81616H20.3797C20.7113 7.81616 21.0292 7.94474 21.2636 8.17362C21.4981 8.40251 21.6298 8.71294 21.6298 9.03663Z" fill="#09161F"/>
+        </svg>
+      </a>
+      <span class="hotspot-mobile-pagination"></span>
+      <a class="hotspot-mobile-button next">
+        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="18" viewBox="0 0 21 18" fill="none">
+            <path d="M20.2006 9.90113L12.9348 17.224C12.7063 17.4513 12.3981 17.5787 12.077 17.5787C11.756 17.5787 11.4478 17.4513 11.2193 17.224C11.1064 17.1106 11.0168 16.9758 10.9557 16.8275C10.8946 16.6791 10.8631 16.5201 10.8631 16.3595C10.8631 16.1988 10.8946 16.0398 10.9557 15.8914C11.0168 15.7431 11.1064 15.6083 11.2193 15.495L16.4163 10.2571H1.58205C1.26089 10.2571 0.952875 10.1285 0.725776 9.89963C0.498677 9.67075 0.371094 9.36032 0.371094 9.03663C0.371094 8.71294 0.498677 8.40251 0.725776 8.17362C0.952875 7.94474 1.26089 7.81616 1.58205 7.81616H16.4163L11.2193 2.5783C10.9918 2.34902 10.864 2.03805 10.864 1.7138C10.864 1.55325 10.8954 1.39427 10.9563 1.24594C11.0173 1.0976 11.1066 0.962827 11.2193 0.849299C11.3319 0.735771 11.4656 0.645716 11.6128 0.584275C11.76 0.522834 11.9177 0.491211 12.077 0.491211C12.3988 0.491211 12.7073 0.620019 12.9348 0.849299L20.2006 8.17213C20.3134 8.28551 20.403 8.42024 20.4641 8.56859C20.5253 8.71695 20.5567 8.876 20.5567 9.03663C20.5567 9.19726 20.5253 9.35631 20.4641 9.50466C20.403 9.65301 20.3134 9.78774 20.2006 9.90113Z" fill="#09161F"/>
+        </svg>
+      </a>
+    </div>
+  `;
 
-    dialog.querySelector('span.hotspot-mobile-pagination').innerHTML = `${prevIndex + 1}/${dialogs.length}`;  
-  });
+
 }
+
 
 /**
  *
@@ -268,7 +265,7 @@ export function addSpot(block, hotspot) {
 
   addDesktopHotspotIcon(hotspot, block, main);
   addDesktopLayover(hotspot, block);
-  updateMobileBeforeAndNextButtons(block);
+  // updateMobileBeforeAndNextButtons(block);
   updateDesktopLayoverBeforeAndNextButtons(block);
 
   addMobileAlternativeCards(hotspot, main);
