721d731b36ebb37aaaf4bf93d9d4b689312b0461
Syb Wartna <sybren.wartna@cognizant.com>
2024-03-29 00:45:48 +0100

    refactor v2-embed, modal, v2-testimonial, video-helpers

--- blocks/v2-embed/v2-embed.js
+++ blocks/v2-embed/v2-embed.js
@@ -1,104 +1,94 @@
 import {
   addVideoConfig,
-  getVideoConfig,
+  createVideo,
+  handleVideoMessage,
 } from '../../scripts/video-helper.js';
+import {
+  AEM_ASSETS,
+} from '../../scripts/constants.js';
 
 const blockName = 'v2-embed';
 
-function logVideoEvent(eventName, videoId, timeStamp) {
-  // eslint-disable-next-line no-console
-  console.info(`[${blockName}] ${eventName} for ${videoId} at ${timeStamp}`);
-}
+const extractAspectRatio = (block) => {
+  const aspectRatioRegex = /aspect-ratio-(\d+)-(\d+)/;
+  const aspectRatioClass = Array.from(block.classList)
+    .find((className) => aspectRatioRegex.test(className));
 
-function formatDebugTime(date) {
-  const timeOptions = {
-    hour12: false,
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-  };
-  const formattedTime = date.toLocaleTimeString('en-US', timeOptions);
-  const milliseconds = date.getMilliseconds().toString().padStart(3, '0');
+  if (!aspectRatioClass) {
+    return null;
+  }
 
-  return `${formattedTime}.${milliseconds}`;
-}
+  const match = aspectRatioClass.match(aspectRatioRegex);
+  if (match) {
+    return {
+      width: parseInt(match[1], 10),
+      height: parseInt(match[2], 10),
+    };
+  }
+
+  return null;
+};
+
+const retrieveVideoConfig = (block, aspectRatio) => ({
+  ...(aspectRatio ? { aspectRatio: `${aspectRatio.width}:${aspectRatio.height}` } : {}),
+  ...(block.querySelector('img')?.getAttribute('src')
+    ? { poster: new URL(block.querySelector('img').getAttribute('src'), window.location.href).href }
+    : {}),
+  autoplay: block.classList.contains('autoplay') ? 'any' : false,
+  muted: block.classList.contains('autoplay'),
+  loop: block.classList.contains('loop'),
+  controls: !block.classList.contains('disable-controls'),
+  controlBar: !block.classList.contains('disable-controls'),
+  disablePictureInPicture: block.classList.contains('disable-picture-in-picture'),
+  language: document.documentElement.lang,
+});
+
+const configureVideo = (block, videoId) => {
+  const config = retrieveVideoConfig(block);
+  Object.entries(config).forEach(([key, value]) => {
+    if (value !== undefined) {
+      addVideoConfig(videoId, { [key]: value });
+    }
+  });
+};
 
 export default function decorate(block) {
-  const isAutoplay = block.classList.contains('autoplay');
-  const isLoopedVideo = block.classList.contains('loop');
-  const isDisableControls = block.classList.contains('disable-controls');
-  const isDisablePictureInPicture = block.classList.contains('disable-picture-in-picture');
-  const link = block.querySelector('a').getAttribute('href');
-  const title = block.querySelector('a').textContent;
-  const regex = /urn:aaid:aem:[0-9a-fA-F-]+/;
-  const match = link.match(regex);
+  const linkElement = block.querySelector('a');
+  const link = linkElement?.getAttribute('href');
+  const title = linkElement?.textContent;
+  const match = link?.match(AEM_ASSETS.videoIdRegex);
 
   if (!link) {
     // eslint-disable-next-line no-console
-    console.warn('[V2 Video Embed block]: There is no video link');
+    console.warn(`[${blockName}]: There is no video link`);
     return;
   }
 
-  if (match) {
-    [block.videoId] = match;
-  } else {
+  if (!match) {
     // eslint-disable-next-line no-console
-    console.warn('[V2 Video Embed block]: Video link is incorrect: ', link);
+    console.warn(`[${blockName}]: Video link is incorrect: ${link}`);
+    return;
   }
 
-  if (isAutoplay) {
-    addVideoConfig(block.videoId, { autoplay: 'any', muted: true });
+  const [videoId] = match;
+  block.videoId = videoId;
+
+  const aspectRatio = extractAspectRatio(block);
+  if (aspectRatio) {
+    block.style.setProperty('--video-aspect-ratio', `${aspectRatio.width}/${aspectRatio.height}`);
   }
 
-  if (isLoopedVideo) {
-    addVideoConfig(block.videoId, { loop: true });
-  }
+  const videoConfig = retrieveVideoConfig(block, aspectRatio);
+  const videoProps = {
+    ...videoConfig,
+    title,
+  };
 
-  if (isDisableControls) {
-    addVideoConfig(block.videoId, { controls: false, controlBar: false });
-  }
+  const videoElement = createVideo(link, `${blockName}__frame`, videoProps, false, videoId);
 
-  if (isDisablePictureInPicture) {
-    addVideoConfig(block.videoId, { disablePictureInPicture: true });
-  }
+  configureVideo(block, videoId);
 
-  window.addEventListener('message', (event) => {
-    if (event.data.name === 'video-config') {
-      if (event.data.videoId === block.videoId) {
-        event.source.postMessage(JSON.stringify(getVideoConfig(block.videoId)), '*');
-      }
-    }
-
-    // TODO: add code to handle other video events
-    if (event.data.type === 'embedded-video-player-event') {
-      const timeStamp = formatDebugTime(new Date());
-      switch (event.data.name) {
-        case 'video-playing':
-          logVideoEvent(event.data.name, event.data.videoId, timeStamp);
-          break;
-        case 'video-play':
-          logVideoEvent(event.data.name, event.data.videoId, timeStamp);
-          break;
-        case 'video-ended':
-          logVideoEvent(event.data.name, event.data.videoId, timeStamp);
-          break;
-        case 'video-loadedmetadata':
-          logVideoEvent(event.data.name, event.data.videoId, timeStamp);
-          break;
-        default:
-          break;
-      }
-    }
-  });
-
-  const videoFrame = document.createRange().createContextualFragment(`
-  <iframe class="${blockName}__frame"
-    allowfullscreen
-    title="${title}"
-    src="${link}">
-  </iframe>
-  `);
-
+  window.addEventListener('message', (event) => handleVideoMessage(event, block.videoId, blockName));
   block.innerHTML = '';
-  block.append(videoFrame);
+  block.append(videoElement);
 }
