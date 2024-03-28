a9c96e876aac226c1d9625d2cdca4a844d30661a
Syb Wartna <sybren.wartna@cognizant.com>
2024-03-28 14:06:34 +0100

    add configuration to custom header

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -419,6 +419,7 @@ export default async function decorate(block) {
   let navPath = `${langCodeMatch ? langCodeMatch[1] : '/'}nav`;
 
   const isCustomHeader = getMetadata('custom-header');
+  const isMobileMenuDisabled = getMetadata('custom-header-mobile-menu').toLowerCase() === 'false';
   if (isCustomHeader) {
     navPath = `${langCodeMatch ? langCodeMatch[1] : ''}${isCustomHeader}`;
     block.classList.add(`${blockClass}__custom`);
@@ -453,8 +454,8 @@ export default async function decorate(block) {
       ${createMainLinks(navigationContainer).outerHTML}
     </div>` : ''}
     <div class="${blockClass}__actions">
-      ${isCustomHeader ? '' : mobileActions().outerHTML}
-      ${isCustomHeader ? decorateCTA(actionsContainer).outerHTML : createActions(actionsContainer).outerHTML}
+      ${isMobileMenuDisabled ? '' : mobileActions().outerHTML}
+      ${isMobileMenuDisabled ? decorateCTA(actionsContainer).outerHTML : createActions(actionsContainer).outerHTML}
     </div>
   `);
 
@@ -607,7 +608,7 @@ export default async function decorate(block) {
     }
   };
 
-  if (!isCustomHeader) {
+  if (!isMobileMenuDisabled) {
     desktopMQ.addEventListener('change', (e) => {
       const isDesktop = e.matches;
 
