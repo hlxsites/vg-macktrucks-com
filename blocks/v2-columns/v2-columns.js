02102ed4d82fbbeb97307ada9c55a4f448e0e19c
SantiagoHomps-NC <103571932+SantiagoHomps-NC@users.noreply.github.com>
2024-04-22 14:32:42 +0200

    '3-links' Columns block variant #582 (#692)

--- blocks/v2-columns/v2-columns.js
+++ blocks/v2-columns/v2-columns.js
@@ -1,15 +1,25 @@
 import { createElement, variantsClassesToBEM } from '../../scripts/common.js';
 
+const blockName = 'v2-columns';
+
+const getLastTextElmts = (block) => {
+  const allTexts = block.querySelectorAll('p');
+  const linksTitle = allTexts[allTexts.length - 1];
+  linksTitle.classList.add('list-title');
+  return linksTitle;
+};
+
 export default async function decorate(block) {
   const blockParent = block.parentElement.parentElement;
-  const blockName = 'v2-columns';
 
-  const variantClasses = ['with-background-image', 'background-plane', 'icon-list'];
+  const variantClasses = ['with-background-image', 'background-plane', 'icon-list', 'navigation-links'];
   variantsClassesToBEM(block.classList, variantClasses, blockName);
 
   const isBackgroundImageVariant = block.classList.contains(`${blockName}--with-background-image`);
   const isIconListVariant = block.classList.contains(`${blockName}--icon-list`);
-  const hasHeader = blockParent.classList.contains('header-with-mark');
+  const is3LinksVariant = block.classList.contains(`${blockName}--navigation-links`);
+  const isListVariant = isIconListVariant || is3LinksVariant;
+  const hasHeader = blockParent.classList.contains('header-with-mark') || blockParent.classList.contains('header-no-mark');
 
   const rows = [...block.querySelectorAll(':scope > div')];
   const columns = [...block.querySelectorAll(':scope > div > div')];
@@ -24,7 +34,7 @@ export default async function decorate(block) {
     const picture = col.querySelector('picture');
     const allTextElmts = col.querySelectorAll('p, ul, ol');
     const bodyElmts = [];
-    const iconList = createElement('div', { classes: `${blockName}--icons` });
+    const linkList = createElement('div', { classes: `${blockName}--links` });
 
     if (picture) {
       col.classList.add(`${blockName}__column--with-image`);
@@ -37,15 +47,18 @@ export default async function decorate(block) {
 
       const isButton = [...e.classList].includes('button-container');
       const isPretitle = nextElmt?.tagName.toLowerCase()[0] === 'h';
-      const isIconList = isIconListVariant && (e.tagName.toLowerCase() === 'ul' || e.tagName.toLowerCase() === 'ol');
+      const hasLinkList = isListVariant && (e.tagName.toLowerCase() === 'ul' || e.tagName.toLowerCase() === 'ol');
 
-      if (!isPretitle && !isButton && !isIconList) {
+      if (hasLinkList) {
+        if (is3LinksVariant) linkList.append(getLastTextElmts(col));
+        linkList.append(e);
+      } else if (!isPretitle && !isButton) {
         bodyElmts.push(e);
-      } else if (isIconList) {
-        iconList.append(e);
       }
     });
-    bodyElmts.forEach((e) => e.classList.add(`${blockName}__body`));
+    bodyElmts.forEach((e) => {
+      if (!e.classList.contains('list-title')) e.classList.add(`${blockName}__body`);
+    });
 
     block.querySelectorAll(`ul.${blockName}__body li`).forEach((item) => {
       item.classList.add('li--hyphen');
@@ -63,15 +76,15 @@ export default async function decorate(block) {
         btnSection.append(btn);
       });
       if (!picture) col.append(btnSection);
-      if (isIconListVariant) {
-        iconList.querySelectorAll('a').forEach((e) => e.classList.add('standalone-link'));
-        col.append(iconList);
+      if (isListVariant) {
+        linkList.querySelectorAll('a').forEach((e) => e.classList.add('standalone-link'));
+        col.append(linkList);
       }
 
       if (hasHeader) {
         const defaultContent = blockParent.querySelector('.default-content-wrapper');
         const header = [...defaultContent.querySelectorAll('h1, h2, h3, h4, h5, h6')];
-        header[0].classList.add(`${blockName}__body-header`, 'with-marker');
+        header[0].classList.add(`${blockName}__body-header`, (!blockParent.classList.contains('header-no-mark') && 'with-marker'));
         bodyElmts[0].insertAdjacentElement('beforebegin', header[0]);
         defaultContent.remove();
       }
