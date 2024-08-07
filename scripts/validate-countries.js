1cde4a2a5a18f1a132cc966b29674b79b2a4696d
santi-homps <santiagohomps@gmail.com>
2024-08-07 12:19:40 -0300

    api key added to constants

--- scripts/validate-countries.js
+++ scripts/validate-countries.js
@@ -1,16 +1,26 @@
-const apiKey = 'AIzaSyAP8IewqHuU8SMz_6tNiIUlbU_l0GFOd1w';
+import { TOOLS_CONFIGS } from './common.js';
+
+const { GOOGLE_API_KEY } = TOOLS_CONFIGS;
 const languageCode = 'en';
 
 export const splitString = (str) => str.split(',').map((item) => item.trim());
 
 export const getUserCountryName = async (lat, lng) => {
-  const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&language=${languageCode}&key=${apiKey}`;
+  const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&language=${languageCode}&key=${GOOGLE_API_KEY}`;
 
   const response = await fetch(apiUrl);
   const locationObj = await response.json();
   const locationString = locationObj.plus_code.compound_code;
 
   return locationString;
+};
+
+const checkForRedirect = (list, country, url) => {
+  // Check if country is included in the list that comes from the metadata
+  if (!list.includes(country)) {
+    const completeUrl = window.location.origin + url;
+    window.location.replace(completeUrl);
+  }
 };
 
 export const validateCountries = async (countries, url) => {
@@ -21,15 +31,12 @@ export const validateCountries = async (countries, url) => {
     const response = await getUserCountryName(latitude, longitude);
     const country = (splitString(response).reverse())[0];
 
-    // Check if country is included in the list that comes from the metadata
-    if (!allowedCountries.includes(country)) {
-      const completeUrl = window.location.origin + url;
-      window.location.replace(completeUrl);
-    }
+    checkForRedirect(allowedCountries, country, url);
   };
   const locationError = (error) => {
     // eslint-disable-next-line no-console
     console.error('Error:', error);
   };
+
   navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
 };
