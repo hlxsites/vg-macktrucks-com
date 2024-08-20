84c8c671e4d12c4c5f297a627f64c3ae1bf7ed4a
Alexis Coelho <alexis.coelho@netcentric.biz>
2024-08-20 12:10:54 +0200

    Refactor v2-dealer-locator, document and get configurations from block #837

--- blocks/v2-dealer-locator/v2-dealer-locator.js
+++ blocks/v2-dealer-locator/v2-dealer-locator.js
@@ -1,9 +1,14 @@
-import { loadScript, loadCSS } from '../../scripts/lib-franklin.js';
+import { loadScript, loadCSS, readBlockConfig } from '../../scripts/lib-franklin.js';
 import { TOOLS_CONFIGS } from '../../scripts/common.js';
 import template from './shared/template.js';
 
 const { GOOGLE_API_KEY } = TOOLS_CONFIGS;
 
+/**
+ * Escapes HTML characters from a string.
+ *
+ * @returns {string} The escaped string
+ */
 function escapeHTML(input) {
   return input.replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
@@ -12,52 +17,112 @@ function escapeHTML(input) {
     .replace(/'/g, '&#039;');
 }
 
+/**
+ * Wrapper around loadScript to avoid the duplication in this file around:
+ * `{ type: 'text/javascript', charset: 'UTF-8' }`
+ *
+ * @returns {Promise} Promise that resolves when the script is loaded
+ */
 const loadComponentScript = (scriptlink) => loadScript(scriptlink, { type: 'text/javascript', charset: 'UTF-8' });
 
-export default async function decorate(block) {
+/**
+ * Gets the zip code from the URL query string.
+ *
+ * @returns {string} Zip code from the URL query string
+ */
+const getZipCode = () => {
   const searchParams = new URLSearchParams(window.location.search);
   const hasZipLocation = searchParams.has('l');
-  const MQ = window.matchMedia('(max-width: 992px)');
-  const isMobile = MQ.matches;
-  // add the zip code to the input search, if it is present
-  const zipCode = hasZipLocation ? escapeHTML(searchParams.get('l')) : null;
-  const datasource = block.textContent.trim();
-  window.locatorConfig = {
-    asist: false,
-    showAsistDialog: true,
-    consolidateFilters: true,
-    selectedBrand: 'mack',
-    dataSource: datasource,
-    apiKey: GOOGLE_API_KEY,
-    // These need to come from a config:
-    vervion: 'la', // 'default' or 'la'
-    country: 'Chile',
-    amenities: ['Appointments Accepted', 'Bilingual Service', 'Driver Lounge', 'Free Pickup and Delivery', 'Hotel Shuttle', 'Internet Service', 'Laundry', 'Showers', 'Telephones', 'Trailer Parking', 'Video Games'],
-  };
-  const isLAMarket = window.locatorConfig.vervion === 'la';
-  const sharedTemplate = template({ zipCode, isMobile, isLAMarket });
 
-  if (window.locatorConfig.vervion === 'la') {
-    loadCSS('/blocks/v2-dealer-locator/la/dealer-locator.css');
+  return hasZipLocation ? escapeHTML(searchParams.get('l')) : '';
+};
+
+/**
+ * Checks if the current screen is mobile.
+ *
+ * @returns {boolean} true if it matched the media query
+ */
+const checkIfIsMobile = () => {
+  const MQ = window.matchMedia('(max-width: 992px)');
+
+  return MQ.matches;
+};
+
+/**
+ * Converts a string with values separated by commas into an array of strings.
+ *
+ * @param {string} amenitiesString - Amenities string to parse. Example:
+ *  "Appointments Accepted, Bilingual Service, Driver Lounge"
+ * @returns {Array.<string>} Extracted config object
+ */
+const parseAmenities = (amenitiesString) => {
+  const amenities = amenitiesString?.split(',').map((amenity) => amenity.trim()) || [];
+
+  return amenities;
+};
+
+/**
+ * Extracts the config object from the block element.
+ *
+ * @param {HTMLElement} block - The block element
+ * @returns {Array.<string>} Extracted config object
+ */
+const getBlockConfigs = (block) => {
+  const config = readBlockConfig(block);
+
+  if (config?.amenities) {
+    config.amenities = parseAmenities(config.amenities);
   }
 
-  block.innerHTML = sharedTemplate;
+  return config;
+};
 
-  loadComponentScript('/blocks/v2-dealer-locator/shared/vendor/jquery.min.js')
-    .then(() => {
-      // These scripts depend on jquery
+export default async function decorate(block) {
+  const zipCode = getZipCode();
+  const isMobile = checkIfIsMobile();
+  const blockConfig = getBlockConfigs(block);
+  const isLAMarket = blockConfig.vervion === 'la';
 
-      if (isLAMarket) {
-        loadComponentScript('/blocks/v2-dealer-locator/la/sidebar-maps.js');
-      } else {
-        loadComponentScript('/blocks/v2-dealer-locator/default/sidebar-maps.js');
-      }
+  // blockConfig.datasource is a required field for the block to work:
+  if (!blockConfig.datasource) {
+    console.error('The block is missing the datasource field in the configuration.');
+  } else {
+    window.locatorConfig = {
+      asist: false,
+      showAsistDialog: true,
+      consolidateFilters: true,
+      selectedBrand: 'mack',
+      dataSource: blockConfig.datasource,
+      apiKey: GOOGLE_API_KEY,
+      vervion: blockConfig.version, // 'default' or 'la'
+      country: blockConfig.country,
+      amenities: blockConfig.amenities,
+    };
 
-      loadComponentScript('/blocks/v2-dealer-locator/default/my-dealer.js');
-    });
+    const sharedTemplate = template({ zipCode, isMobile, isLAMarket });
 
-  loadComponentScript('/blocks/v2-dealer-locator/shared/vendor/moment.js')
-    .then(() => {
-      loadComponentScript('/blocks/v2-dealer-locator/shared/vendor/moment-timezone.min.js');
-    });
+    if (window.locatorConfig.vervion === 'la') {
+      loadCSS('/blocks/v2-dealer-locator/la/dealer-locator.css');
+    }
+
+    block.innerHTML = sharedTemplate;
+
+    loadComponentScript('/blocks/v2-dealer-locator/shared/vendor/jquery.min.js')
+      .then(() => {
+        // These scripts depend on jquery
+
+        if (isLAMarket) {
+          loadComponentScript('/blocks/v2-dealer-locator/la/sidebar-maps.js');
+        } else {
+          loadComponentScript('/blocks/v2-dealer-locator/default/sidebar-maps.js');
+        }
+
+        loadComponentScript('/blocks/v2-dealer-locator/default/my-dealer.js');
+      });
+
+    loadComponentScript('/blocks/v2-dealer-locator/shared/vendor/moment.js')
+      .then(() => {
+        loadComponentScript('/blocks/v2-dealer-locator/shared/vendor/moment-timezone.min.js');
+      });
+  }
 }
