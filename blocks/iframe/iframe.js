7df43fd3a045126084e3736f13cb902146d8c9cd
Jonatan Lledo <117984204+jonatan-lledo-netcentric@users.noreply.github.com>
2024-08-23 17:22:49 +0200

    subscribe button & form not render if is disabled #818 (#841)
    * subscribe button and form not render if is disabled #818
    
    * simplify config to check if the button is allowed or not #818
    
    * add subs btns if they are allowed #818

--- blocks/iframe/iframe.js
+++ blocks/iframe/iframe.js
@@ -1,6 +1,10 @@
+import { createElement } from '../../scripts/common.js';
+
 export default async function decorate(block) {
-  const iframe = document.createElement('iframe');
   const link = block.querySelector('a')?.getAttribute('href');
+  const iframe = createElement('iframe', {
+    props: { frameborder: 0, src: link },
+  });
   const fixedHeightClass = [...block.classList].find((el) => /[0-9]+px/.test(el));
   const maxWidthClass = [...block.classList].find((el) => /width-[0-9]+px/.test(el));
 
@@ -8,10 +12,8 @@ export default async function decorate(block) {
     iframe.height = fixedHeightClass;
   }
   if (maxWidthClass) {
-    const maxWidht = maxWidthClass.split('width-')[1];
-    iframe.style.maxWidth = maxWidht;
+    const maxWidth = maxWidthClass.split('width-')[1];
+    iframe.style.maxWidth = maxWidth;
   }
-  iframe.src = link;
-  iframe.setAttribute('frameborder', 0);
   block.replaceChildren(iframe);
 }
