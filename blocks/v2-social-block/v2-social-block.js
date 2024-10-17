eca7880568ae3202f811d18c6f105300401d06c8
santi-homps <santiagohomps@gmail.com>
2024-10-17 15:04:37 -0300

    block refactored

--- blocks/v2-social-block/v2-social-block.js
+++ blocks/v2-social-block/v2-social-block.js
@@ -1,109 +1,96 @@
 import {
   getTextLabel,
   unwrapDivs,
-  createElement,
   decorateIcons,
 } from '../../scripts/common.js';
 
 const blockName = 'v2-social-block';
+const docRange = document.createRange();
 
 const LABELS = {
   COPIED: getTextLabel('tooltip copied text'),
 };
+
 const CLASSES = {
   title: `${blockName}__title`,
-  listWrapper: `${blockName}__list-wrapper`,
   list: `${blockName}__list`,
-  button: `${blockName}__button`,
+  listItem: `${blockName}__list-item`,
   tooltipTop: ['tooltip', 'tooltip--top'],
   tooltipShow: 'show',
 };
 
-const buildTemplateBlock = (block) => {
-  const shareConfigs = [
-    ['twitter', 'https://twitter.com/intent/tweet?url='],
-    ['linkedin', 'https://www.linkedin.com/cws/share?url='],
-    ['fb', 'https://www.facebook.com/sharer/sharer.php?u='],
-  ];
-  const fragment = document.createDocumentFragment();
+const TEMPLATE_LINK_CONFIGS = [
+  ['twitter', 'https://twitter.com/intent/tweet?url='],
+  ['linkedin', 'https://www.linkedin.com/cws/share?url='],
+  ['fb', 'https://www.facebook.com/sharer/sharer.php?u='],
+];
 
-  shareConfigs.forEach((link) => {
-    const linkElement = createElement('li');
-    linkElement.innerHTML = `
-      <a class='${CLASSES.button}'>
-        <span class='icon icon-${link[0]}'></span>
-      </a>
-    `;
-    linkElement.querySelector('a').addEventListener('click', () => {
-      window.open(`${link[1]}${window.location.href}`, '_blank');
-    });
-
-    fragment.appendChild(linkElement);
-  });
+const buildTemplateBlock = (links) => {
+  const fragment = docRange.createContextualFragment(`
+    <ul class='${CLASSES.list}'>
+      ${links.map((link) => `
+        <li class='${CLASSES.listItem}'>
+          <a href='${link[1]}${window.location.href}' target='_blank'>
+            <span class='icon icon-${link[0]}'></span>
+          </a>
+        </li>
+        `).join('')}
+    </ul>
+  `);
   decorateIcons(fragment);
-
-  const shareLinkList = createElement('ul');
-  shareLinkList.classList.add(CLASSES.list);
-
-  shareLinkList.appendChild(fragment);
-
-  const isFirstOrLast = document.querySelectorAll(`.${CLASSES.list}`).length === 0;
-
-  const socialWrapper = createElement('div');
-  socialWrapper.classList.add(CLASSES.listWrapper);
-
-  socialWrapper.appendChild(shareLinkList);
-
-  block.classList.add(isFirstOrLast ? 'first' : 'last');
-  block.appendChild(socialWrapper);
+  return fragment;
 };
 
-const buildDefaultBlock = (block) => {
-  const headings = block.querySelectorAll('h1, h2, h3, h4, h5, h6');
-  [...headings].forEach((heading) => heading.classList.add(CLASSES.title));
+const buildDefaultBlock = (heading = undefined, links = []) => {
+  heading?.classList.add(CLASSES.title);
+  const fragment = docRange.createContextualFragment(`
+    ${heading ? heading.outerHTML : ''}
+    <ul class='${CLASSES.list}'>
+      ${links.map((anchor) => `<li class='${CLASSES.listItem}'>${anchor.outerHTML}</li>`).join('')}
+    </ul>
+  `);
+  return fragment;
+};
 
-  const socialWrapper = block.querySelector(':scope > div');
-  socialWrapper.classList.add(CLASSES.listWrapper);
+const addTooltip = (link) => {
+  const anchorEl = link.parentElement;
+  anchorEl.dataset.tooltip = LABELS.COPIED;
+  anchorEl.addEventListener('click', async (e) => {
+    e.preventDefault();
+    try {
+      await navigator.clipboard.writeText(`${anchorEl.href}`);
+      anchorEl.classList.add(CLASSES.tooltipShow);
 
-  const list = socialWrapper.querySelector('ul');
-  list.classList.add(CLASSES.list);
-
-  [...list.children].forEach((item) => {
-    const anchor = item.querySelector('a');
-    if (anchor) {
-      anchor.className = CLASSES.button;
-    }
-
-    const copyLink = item.querySelector('.icon-link');
-    if (copyLink) {
-      anchor.dataset.tooltip = LABELS.COPIED;
-
-      anchor.addEventListener('click', async (e) => {
-        e.preventDefault();
-        try {
-          await navigator.clipboard.writeText(`${anchor.href}`);
-          anchor.classList.add(CLASSES.tooltipShow);
-
-          setTimeout(() => {
-            anchor.classList.remove(CLASSES.tooltipShow);
-          }, 1000);
-        } catch (err) {
-          /* eslint-disable-next-line no-console */
-          console.error('Failed to copy: ', err);
-        }
-      });
-      anchor.classList.add(...CLASSES.tooltipTop);
+      setTimeout(() => {
+        anchorEl.classList.remove(CLASSES.tooltipShow);
+      }, 1000);
+    } catch (err) {
+      /* eslint-disable-next-line no-console */
+      console.error('Failed to copy: ', err);
     }
   });
+  anchorEl.classList.add(...CLASSES.tooltipTop);
 };
 
 export default async function decorate(block) {
-  const isMagazineTemplate = document.querySelector('body').classList.contains('v2-magazine');
+  let fragment;
+  const isMagazineTemplate = document.body.classList.contains('v2-magazine');
+
   if (isMagazineTemplate) {
-    buildTemplateBlock(block);
+    fragment = buildTemplateBlock(TEMPLATE_LINK_CONFIGS);
   } else {
-    buildDefaultBlock(block);
+    const heading = block.querySelectorAll('h1, h2, h3, h4, h5, h6')[0];
+    const links = Array.from(block.querySelectorAll('a'));
+
+    fragment = buildDefaultBlock(heading, links);
+
+    const copyIcon = fragment.querySelector('.icon-link');
+    if (copyIcon) {
+      addTooltip(copyIcon);
+    }
   }
+  block.innerHTML = '';
+  block.appendChild(fragment);
 
   unwrapDivs(block);
 }
